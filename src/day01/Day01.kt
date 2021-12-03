30212bfe4c9af206a5b8c0f52e694e109ab01cd9
Antoine Averlant <averlant.antoine@gmail.com>
2021-12-03 02:38:04 +0100

    clean Day02

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -13,8 +13,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputInt("day01/Day01_test")
-    println("Part 1 test = ${part1(testInput)}")
-    println("Part 2 test = ${part2(testInput)}")
+    println("Test part 1 = ${part1(testInput) == 7}")
+    println("Test part 2 = ${part2(testInput) == 5}")
 
     val input = readInputInt("day01/Day01")
     println("Part 1 = ${part1(input)}")
--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -4,43 +4,32 @@ import readInput
 
 fun main() {
     fun String.getCommandValue(command: String): Int {
-        return this.removePrefix(command).trim().toInt()
+        return if (this.contains(command)) this.removePrefix(command).trim().toInt() else 0
     }
 
-    fun getSumInstruction(input: List<String>, command: String): Int {
-       return input.filter { it.contains(command) }.map { it.getCommandValue(command) }.sum()
+    fun List<String>.getSumValueForCommand(command: String): Int {
+        return this.filter { it.contains(command) }.map { it.getCommandValue(command) }.sum()
     }
 
     fun part1(input: List<String>): Int {
-        val sumForward = getSumInstruction(input, "forward")
-        val sumDown = getSumInstruction(input, "down")
-        val sumUp = getSumInstruction(input, "up")
-
-        return (sumForward * (sumDown - sumUp))
+        val horizontal = input.getSumValueForCommand("forward")
+        val aim = (input.getSumValueForCommand("down") - input.getSumValueForCommand("up"))
+        return (horizontal * aim)
     }
 
-    // TODO find better solution
     fun part2(input: List<String>): Int {
         var aim = 0
         var depth = 0
         var horizontal = 0
 
         input.forEach { command ->
-            when{
-                command.contains("forward") -> {
-                    val number = command.getCommandValue("forward")
-                    horizontal += number
-                    if(aim > 0) {
-                        depth += aim * number
-                    }
-                }
-                command.contains("down") -> {
-                    aim += command.getCommandValue("down")
-                }
-                command.contains("up") -> {
-                    aim -= command.getCommandValue("up")
-                }
+            val value = command.getCommandValue("forward")
+            horizontal += value
+            if (aim > 0) {
+                depth += aim * value
             }
+            aim += command.getCommandValue("down")
+            aim -= command.getCommandValue("up")
         }
 
         return (depth * horizontal)
@@ -48,8 +37,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day02/Day02_test")
-    println("Part 1 test = ${part1(testInput)}")
-    println("Part 2 test = ${part2(testInput)}")
+    println("Test part 1 = ${part1(testInput) == 150}")
+    println("Test part 2 = ${part2(testInput) == 900}")
 
     val input = readInput("day02/Day02")
     println("Part 1 = ${part1(input)}")
